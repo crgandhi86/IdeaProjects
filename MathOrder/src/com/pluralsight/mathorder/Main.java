608b7a17f46499effba8bb56b9aeb0d9ea7d232d
crgandhi86 <crgandhi86@users.noreply.github.com>
2018-08-19 08:59:20 -0700

    Initial commit

---
+++ MathOrder/src/com/pluralsight/mathorder/Main.java
@@ -0,0 +1,18 @@
+package com.pluralsight.mathorder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int varA = 21;
+	    int varB = 6;
+	    int varC = 3;
+	    int varD = 1;
+
+	    int result1 = varA - varB / varC;
+
+        int result2 = (varA - varB) / varC;
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
